382a9dddb0d479209a74eed31cf9f57412bbc3d6
Ahmed sayed <ahmedsaye23156@gmail.comm>
2023-02-16 06:09:58 -0800

    new test

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,15 @@
-#include  <stdio.h>
-int main(){
-
-        printf("size of a char is :%d byte(s)\n",sizeof(char));
-        printf("size of a int is :%d byte(s)\n",sizeof(int));
-        printf("size of long int is :%d byte(s)\n",sizeof(long int));
-        printf("size of long long int is %d byye(s)\n",sizeof(long long int));
-        printf("size of float is %d byte(s)\n",sizeof(float));
-        return (0);
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
-
