7ccbcc01484f86c0f6ef651856daf946584e335e
Ahmed sayed <ahmedsaye23156@gmail.comm>
2023-02-16 05:58:28 -0800

    printf function

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include  <stdio.h>
+int main(){
+
+	printf("size of a char is :%d bytes \n",sizeof(char));
+ printf("size of a int is :%d bytes \n",sizeof(int));
+printf("size of long int is :%d bytes \n ",sizeof(long int));
+		printf("size of long long int is %d byyes \n",sizeof(long long int));
+			printf("size of float is %d bytes \n",sizeof(float));
+	return (0);
+}
