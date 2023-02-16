bc30edf5efa78f5baec58fa09d1824007b0c2fc7
Ahmed sayed <ahmedsaye23156@gmail.comm>
2023-02-16 06:07:52 -0800

    test git commit -m test git commit -m test

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include  <stdio.h>
 int main(){
 
-        printf("size of a char is :%d byte(s) \n",sizeof(char));
- printf("size of a int is :%d byte(s) \n",sizeof(int));
-printf("size of long int is :%d byte(s) \n ",sizeof(long int));
-                printf("size of long long int is %d byye(s) \n",sizeof(long long int));
-                        printf("size of float is %d byte(s) \n",sizeof(float));
+        printf("size of a char is :%d byte(s)\n",sizeof(char));
+        printf("size of a int is :%d byte(s)\n",sizeof(int));
+        printf("size of long int is :%d byte(s)\n",sizeof(long int));
+        printf("size of long long int is %d byye(s)\n",sizeof(long long int));
+        printf("size of float is %d byte(s)\n",sizeof(float));
         return (0);
 }
 
