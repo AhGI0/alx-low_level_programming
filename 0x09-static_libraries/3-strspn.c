4bc5e6bc78abcd6ccf238e9bf83bf3075e11d2ff
Ahemd <ahmedsaye23156@gmail.com>
2023-03-10 15:12:05 +0200

    new

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,29 +1,35 @@
 #include "main.h"
+
 /**
- *_strspn - search the number of bytes in the initial
- * segment of s which consist only of bytes from accept
- *@s:segment targeted
- *@accept:reference bytes container
- *Return:returns the number of bytes in the initial
- * segment of s which consist only of bytes from accept
+ * *_strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match in s
+ *
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int bytes = 0;
-int i;
-while (*s)
-{
-for (i = 0; accept[i]; i++)
-{
-if (accept[i] == *s)
-{
-bytes++;
-break;
-}
-else if ((accept[i + 1]) == '\0')
-return (bytes);
-}
-s++;
-}
-return (bytes);
+	int i, j, f, flag;
+
+	f = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				f++;
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+		{
+			return (f);
+		}
+	}
+
+	return (0);
 }
