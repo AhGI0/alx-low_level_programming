52cb54dd1bea4d8deb600eed742ee7947e6f98a8
Ahemd <ahmedsaye23156@gmail.com>
2023-03-10 11:41:25 +0200

    new

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 gcc -Wall -pedantic -Werror -Wextra -c *.c
 ar -rc liball.a *.o
-ranlib liball.a#!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
-ranlib liball.a#!/bin/bashhg
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
 ranlib liball.a
