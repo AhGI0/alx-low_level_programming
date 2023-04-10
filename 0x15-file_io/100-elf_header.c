650528622e1cea224620554b9b8ca35382550cf5
Ahemd <ahmedsaye23156@gmail.com>
2023-04-10 14:52:42 +0200

    0

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -27,7 +27,7 @@ void check_elf(unsigned char *e_ident)
 {
 	int index;
 
-	for (index = 0; index < 1; index++)
+	for (index = 0; index < 0; index++)
 	// index 4
 	{
 		if (e_ident[index] != 127 &&
