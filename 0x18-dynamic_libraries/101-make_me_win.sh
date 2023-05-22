7604072a6401113e0243121179004c9c8c17042b
Ahemd <ahmedsaye23156@gmail.com>
2023-05-22 13:54:57 +0300

    i'm like a star

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ahmedsayedjobran1/alx-low_level_programming/tree/master/0x18-dynamic_libraries/iwin.so
-export LD_PRELOAD=/tmp/iwin.so
+wget -P /tmp https://github.com/ahmedsayedjobran1/alx-low_level_programming/tree/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
